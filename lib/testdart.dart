54065a3d16bbd1d9b042aa5448054dc4d8b0bbe0
long <longbeo1605@gmail.com>
2022-10-24 22:32:05 +0700

    Initial commit

---
+++ lib/testdart.dart
@@ -0,0 +1,17 @@
+void main() {
+  double a = 10,
+      b = -50,
+      c = 11,
+      d = 6,
+      e = 9;
+  /*List<double> numbers = [a,b,c];
+  numbers.sort();*/
+
+  List<double> numbers = [a, b, c, d, e];
+  if (numbers.isNotEmpty) {
+    //numbers.sort((a,b) => (a - b).toInt()); // sắp xếp xuôi chiều ( bé đến lớn )
+    numbers.sort((a, b) => b.compareTo(a)); // sắp xếp ngược chiều ( lớn đến bé )
+    double max = numbers.last;
+    print(max);
+  }
+}
